69b0c67920f1dd6aade41ab20a988d859cbd950a
hervebu <h.buterak@gmail.com>
2020-10-16 11:07:58 +0200

    fix(auth): fix adding users to database
    fix custom id assigned to new users in the firebase

--- javascript-files/signup.js
+++ javascript-files/signup.js
@@ -3,69 +3,82 @@ var signUpForm = document.getElementById('signup-form');
 
 var usersCollection = firebase.database().ref('users');
 
+
+
 signUpForm.addEventListener('submit',submitSignupFunc);
 
 function submitSignupFunc(e) {
     e.preventDefault();
-    var newUserName =document.getElementById('name-input').value;;
-    var newUserEmail =document.getElementById('email-input').value;
-    var newUserDOB =document.getElementById('birthdate-input').value;
-    var newUserPhone =document.getElementById('phone-input').value;
-    var newUserPassword =document.getElementById('password-input').value;
+    var userId = 0;
+    var newUserName = document.getElementById('name-input').value;;
+    var newUserEmail = document.getElementById('email-input').value;
+    var newUserDOB = document.getElementById('birthdate-input').value;
+    var newUserPhone = document.getElementById('phone-input').value;
+    var newUserPassword = document.getElementById('password-input').value;
     var adminAccount = document.getElementById('admin-check');
     var guestUserAccount = document.getElementById('guest-check');
-    var newPasswordConfirm=document.getElementById('passconfirm-input').value;
+    var newPasswordConfirm= document.getElementById('passconfirm-input').value;
+    var usersDatabase = firebase.database().ref('users');
+    usersDatabase.once('value', function (snapshot) {
+        if (!snapshot.exists()) {
+            articleTotalNum = 0;
+        }else {
+            var usersNum = snapshot.numChildren();
+        }
+        if (newUserPassword === newPasswordConfirm) {
+            if (adminAccount.checked  && !guestUserAccount.checked) {
+                typeOfaccount = 'admin';
+                firebase.auth().createUserWithEmailAndPassword(newUserEmail, 
+                    newUserPassword).then(function(){
+                    createUserAccount (usersNum,newUserName,newUserEmail,newUserDOB,newUserPhone,
+                        typeOfaccount,newUserPassword);
+                    signUpForm.reset();
+                    window.location.assign('../Ui-templates/admin-page.html');
+                        }).catch(function(error) {
+                                // Handle Errors here.
+                            var errorCode = error.code;
+                            var errorMessage = error.message;
+                            console.log(errorCode,errorMessage);
+                        });    
+            }else if(guestUserAccount.checked  && !adminAccount.checked) {
+                typeOfaccount = 'guestUser';
+                firebase.auth().createUserWithEmailAndPassword(newUserEmail,
+                    newUserPassword).then(function(){
+                    createUserAccount (usersNum,newUserName,newUserEmail,newUserDOB,
+                        newUserPhone,typeOfaccount,newUserPassword);
+                    signUpForm.reset();
+                        }).catch(function(error) {
+                                // Handle Errors here.
+                            var errorCode = error.code;
+                            var errorMessage = error.message;
+                            console.log(errorCode,errorMessage);
+                        });
+            } else if (adminAccount.checked && guestUserAccount.checked){
+                alert('You have to choose one type of account');
+            } else {
+                alert('choose a type of account');
+            }            
 
-    if (newUserPassword === newPasswordConfirm) {
-        if (adminAccount.checked  && !guestUserAccount.checked) {
-            typeOfaccount = 'admin';
-            firebase.auth().createUserWithEmailAndPassword(newUserEmail, newUserPassword).then(function(){
-                createUserAccount (newUserName,newUserEmail,newUserDOB,newUserPhone,typeOfaccount,newUserPassword);
-                signUpForm.reset();
-                window.location.assign('../Ui-templates/admin-page.html');
-                    }).catch(function(error) {
-                            // Handle Errors here.
-                        var errorCode = error.code;
-                        var errorMessage = error.message;
-                        console.log(errorCode,errorMessage);
-                    });    
-        }else if(guestUserAccount.checked  && !adminAccount.checked) {
-            typeOfaccount = 'guestUser';
-            firebase.auth().createUserWithEmailAndPassword(newUserEmail, newUserPassword).then(function(){
-                createUserAccount (newUserName,newUserEmail,newUserDOB,newUserPhone,typeOfaccount,newUserPassword);
-                signUpForm.reset();
-                    }).catch(function(error) {
-                            // Handle Errors here.
-                        var errorCode = error.code;
-                        var errorMessage = error.message;
-                        console.log(errorCode,errorMessage);
-                    });
-        } else if (adminAccount.checked && guestUserAccount.checked){
-            alert('You have to choose one type of account');
+
+
         } else {
-            alert('choose a type of account');
-        }            
-
-
-
-    } else {
-        alert(`${newUserName}, correctly confirm your password.`)
-    }
-    
+            alert(`${newUserName}, correctly confirm your password.`)
+        }
+    });
 }
 
 
 
 
-function createUserAccount (newUserName,newUserEmail,newUserDOB,newUserPhone,typeOfaccount,newUserPassword) {
-    var newUserCollection = usersCollection.push();
 
-    newUserCollection.set({
-        name:newUserName,
-        email:newUserEmail,
-        dateOfBirth:newUserDOB,
-        phoneNumber:newUserPhone,
-        accountType:typeOfaccount,
-        password:newUserPassword
-    });
+function createUserAccount (usersNum,newUserName,newUserEmail,newUserDOB,newUserPhone,
+    typeOfaccount,newUserPassword) {   
+    usersCollection.child(usersNum + 1).set({
+                name:newUserName,
+                email:newUserEmail,
+                dateOfBirth:newUserDOB,
+                phoneNumber:newUserPhone,
+                accountType:typeOfaccount,
+                password:newUserPassword
+            });
 }
