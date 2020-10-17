2942f8fdfc8c4debd19cf6275e2d7ee39f9b013c
hervebu <h.buterak@gmail.com>
2020-10-17 21:20:04 +0200

    chore: add logic to add articles to firebase
    add logic that allows ascending in indices of snaps

--- javascript-files/createblog.js
+++ javascript-files/createblog.js
@@ -1,32 +1,44 @@
 
-var articlesDatabase = firebase.database().ref('articles');
-let addArticleForm = document.getElementById('article-form');
+var addArticleForm = document.getElementById('article-form');
 addArticleForm.addEventListener("submit",submitFun);
- 
 
-function submitFun(e) {
+ function submitFun(e) {
     e.preventDefault();
+    var articlesDatabase = firebase.database().ref('articles');
+    articlesDatabase.once('value', function (snapshot) {
+        if (!snapshot.exists()) {
+            articleTotalNum = 0;
+        }else {
+            var articleTotalNum = snapshot.numChildren();
+        }
+        var articleTitle = document.getElementById('blog-title').value;
+        var articleContent= document.getElementById('blog-body').value;
+        var dateAndTimeCreated = `${new Date().getDate()}/${new Date().getMonth() + 1}` 
+           + `/${new Date().getFullYear()}  ${new Date().getHours()}:${new Date().getMinutes()}`;
+                
+        saveArticle(articleTotalNum,articleTitle,articleContent,dateAndTimeCreated);
+        addArticleForm.reset();
+        alert(`The blog "${articleTitle}" has been created.`);
+                
+        function saveArticle (articleTotalNum,articleTitle,articleContent,dateAndTimeCreated) {
+            articlesDatabase.child(articleTotalNum + 1).set({
+                   Title:articleTitle,
+                   blogContent:articleContent,
+                   timeCreated:dateAndTimeCreated   
+                });
+    
+        }    
+ });
+}
+
+document.getElementById('logout-btn').addEventListener('click',logoutFun);
+
+function logoutFun() {
+    firebase.auth().signOut().then(function() {
+      window.location.replace("login.html");
+       }).catch(function(error) {
+        // An error happened.
+        });   
+}
+
      
-
-    var articleTitle = document.getElementById('blog-title').value;
-    var  articleContent= document.getElementById('blog-body').value;
-    var dateAndTimeCreated = `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}  ${new Date().getHours()}:${new Date().getMinutes()}`;
-    
-
-    saveArticle(articleTitle,articleContent,dateAndTimeCreated);
-     addArticleForm.reset();
-    alert(`The blog ${articleTitle} has been created.`);
-    
- }
- 
-
- function saveArticle (articleTitle,articleContent,dateAndTimeCreated) {
-     var newArticleDatabase = articlesDatabase.push();
-
-     newArticleDatabase.set({
-         Title:articleTitle,
-         blogContent:articleContent,
-         timeCreated:dateAndTimeCreated
-         
-     });
- }
